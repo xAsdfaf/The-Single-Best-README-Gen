f390e995ff3be29c9f6294f49627f3b919601fce
xAsdfaf <georgejordanjr8@gmail.com>
2023-02-12 12:49:22 -0600

    added questions with input validation

--- index.js
+++ index.js
@@ -6,51 +6,61 @@ const questions = inquirer.prompt([
     {
         type: 'input',
         message: 'What is your GitHub username?',
-        name: 'name'
+        name: 'name',
+        // Checks for user input, returns error if no input is given.
+        validate: (value)=>{ if(value){return true} else {return "You must enter a value to continue."}}
     },
     {
         type: 'input',
         message: 'What is the title of your project?',
-        name: 'title'
+        name: 'title',
+        validate: (value)=>{ if(value){return true} else {return "You must enter a value to continue."}}
     },
     {
         type: 'input',
         message: 'How would you describe your project?',
-        name: 'description'
+        name: 'description',
+        validate: (value)=>{ if(value){return true} else {return "You must enter a value to continue."}}
     },
     {
         type: 'input',
         message: 'What are your installation instructions?',
-        name: 'install'
+        name: 'install',
+        validate: (value)=>{ if(value){return true} else {return "You must enter a value to continue."}}
     },
     {
         type: 'input',
         message: 'What instructions should be followed?',
-        name: 'usage'
+        name: 'usage',
+        validate: (value)=>{ if(value){return true} else {return "You must enter a value to continue."}}
     },
     {
         type: 'input',
         message: 'Is there any credit that is needed to be given?',
-        name: 'credit'
+        name: 'credit',
+        validate: (value)=>{ if(value){return true} else {return "You must enter a value to continue."}}
     },
     {
         type: 'input',
         message: 'Any contribution guidelines?',
-        name: 'contribution'
+        name: 'contribution',
+        validate: (value)=>{ if(value){return true} else {return "You must enter a value to continue."}}
     },
     {
         type: 'list',
         message: 'What license would you like to use?',
         choices: ['The Apache License 2.0', 'The GNU General Public License v3.0', 'The MIT License'],
-        name: 'license'
+        name: 'license',
+        validate: (value)=>{ if(value){return true} else {return "You must enter a value to continue."}}
     },
     {
         type: 'input',
         message: 'Any testing instructions?',
-        name: 'test'
+        name: 'test',
+        validate: (value)=>{ if(value){return true} else {return "You must enter a value to continue."}}
     },
 ]
-);
+)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
